68a0ff8adba51133e1db7a1c03f90f191d652873
Andre Bastos <afb.bastos@gmail.com>
2025-02-24 13:38:18 +0100

    Initial commit

---
+++ src/main/java/fr/eni/encheresgr4/EncheresGr4Application.java
@@ -0,0 +1,13 @@
+package fr.eni.encheresgr4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncheresGr4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncheresGr4Application.class, args);
+	}
+
+}
---
+++ src/test/java/fr/eni/encheresgr4/EncheresGr4ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.encheresgr4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncheresGr4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
